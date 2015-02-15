f110461cb754464d4a40b3e3df27bf32c3b39e99
Azure-Vani <echooatmeal@gmail.com>
2015-02-15 18:55:27 +0800

    add unregister filesystem

--- init.c
+++ init.c
@@ -13,6 +13,8 @@ int __init ftpfs_init(void) {
 
 void __exit ftpfs_fini(void) {
     pr_debug("ftpfs module unloaded\n");
+
+    unregister_filesystem(&ftp_fs_type);
 }
 
 module_init(ftpfs_init); // Maybe fs_initcall() is more appropriate
